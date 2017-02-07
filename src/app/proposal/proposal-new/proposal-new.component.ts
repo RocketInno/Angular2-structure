b9a57bbedc873a73ad5284a021401aa99b604200
beto8 <alberto@collectivemedia.mx>
2017-02-07 17:46:10 -0600

    Agregar estilo a form

--- src/app/proposal/proposal-new/proposal-new.component.ts
+++ src/app/proposal/proposal-new/proposal-new.component.ts
@@ -5,7 +5,7 @@ import { Proposal } from './../proposal';
 @Component({
 	selector: 'app-proposal-new',
 	templateUrl: './proposal-new.component.html',
-	styleUrls: ['./proposal-new.component.css']
+	styleUrls: ['./proposal-new.component.scss']
 })
 export class ProposalNewComponent implements OnInit {
 	private proposal = Proposal;
